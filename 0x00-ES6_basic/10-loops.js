e2b4c0acaba0774b99aeb3e693428c846027c03b
bugfix.exe <bugfix.exe@gmail.com>
2024-09-30 15:49:13 +0100

    debugging problen 10-loop

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,7 +1,8 @@
 export default function appendToEachArrayValue(array, appendString) {
   for (const idx of array) {
     const index = array.indexOf(idx);
-    array[index] = appendString + idx;
+    const newString = appendString + idx;
+    array.splice(index, 1, newString);
   }
 
   return array;
