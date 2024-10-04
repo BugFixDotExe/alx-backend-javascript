1d235c490c44dc059e302d506b6b044dc3039f8d
bugfix.exe <bugfix.exe@gmail.com>
2024-10-04 15:38:38 +0100

    fixed code lint error for the moduele 1-make_classroom

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -1,10 +1,10 @@
 import ClassRoom from './0-classroom';
 
-export default function initializeRooms () {
-    const classRoomObj = []
-    const classRoom01 = new ClassRoom(19);
-    const classRoom02 = new ClassRoom(20);
-    const classRoom03 = new ClassRoom(34);
-    classRoomObj.push(classRoom01, classRoom02, classRoom03)
-    return (classRoomObj);
+export default function initializeRooms() {
+  const classRoomObj = [];
+  const classRoom01 = new ClassRoom(19);
+  const classRoom02 = new ClassRoom(20);
+  const classRoom03 = new ClassRoom(34);
+  classRoomObj.push(classRoom01, classRoom02, classRoom03);
+  return (classRoomObj);
 }
