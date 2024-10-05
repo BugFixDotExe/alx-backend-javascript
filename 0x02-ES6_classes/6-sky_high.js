a5cd11eea60ebec94dc336ba315670c179201e8c
bugfix.exe <bugfix.exe@gmail.com>
2024-10-05 13:14:08 +0100

    debugged the 6-sky_high, throw new TypeError not return

--- 0x02-ES6_classes/6-sky_high.js
+++ 0x02-ES6_classes/6-sky_high.js
@@ -2,16 +2,22 @@ import Building from './5-building';
 
 export default class SkyHighBuilding extends Building {
   constructor(sqft, floors) {
-    if (!Number.isInteger(floors)) { return new TypeError('Floors must be a Number'); }
+    if (!Number.isInteger(floors)) {
+      throw new TypeError('Floors must be a Number');
+    }
     super(sqft);
     this._floors = floors;
   }
 
-  get sqft() { return super.sqft; }
+  get sqft() {
+    return this._sqft;
+  }
 
-  get floors() { return this._floors; }
+  get floors() {
+    return this._floors;
+  }
 
   evacuationWarningMessage() {
-    return (`Evacuate slowly the ${this.floors} floors.`);
+    return `Evacuate slowly the ${this.floors} floors.`;
   }
 }
