f5a99b36c9246836e9f29e9b300f1dfba6d56fca
bugfix.exe <bugfix.exe@gmail.com>
2024-10-05 12:28:53 +0100

    debugging the 6-sky_high

--- 0x02-ES6_classes/6-sky_high.js
+++ 0x02-ES6_classes/6-sky_high.js
@@ -3,12 +3,11 @@ import Building from './5-building';
 export default class SkyHighBuilding extends Building {
   constructor(sqft, floors) {
     if (!Number.isInteger(floors)) { return new TypeError('Floors must be a Number'); }
-    if (!Number.isInteger(sqft)) { return new TypeError('Floors must be a Number'); }
     super(sqft);
     this._floors = floors;
   }
 
-  get sqft() { return this._sqft; }
+  get sqft() { return super.sqft; }
 
   get floors() { return this._floors; }
 
