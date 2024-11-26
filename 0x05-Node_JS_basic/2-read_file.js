29808a788549e7e74c6b4ae6ccb7b63b7035c02f
BugFixDotExe <bugfix.exe@gmail.com>
2024-11-26 13:36:33 +0100

    debugged the style error

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -20,28 +20,23 @@ function countStudents(pathToFile) {
     });
     console.log(`Number of students: ${newData.length - 2}`);
     process.stdout.write(`Number of students in CS: ${numberOfCS.length}. List: `);
-     for (let i = 0; i < numberOfCS.length; i++)
-	  {
-		  if (i === numberOfCS.length - 1)
-		  {
-			  process.stdout.write(numberOfCS[i]);
-		  }
-		  else{
-		  	process.stdout.write(numberOfCS[i] + ', ');
-		  }
-	  }
-     process.stdout.write('\n');
+    for (let i = 0; i < numberOfCS.length; i += 1) {
+      if (i === numberOfCS.length - 1) {
+        process.stdout.write(numberOfCS[i]);
+      } else {
+        process.stdout.write(`${numberOfCS[i]}, `);
+      }
+    }
+    process.stdout.write('\n');
     process.stdout.write(`Number of students in SWE: ${numberOfSWE.length}. List: `);
-	  for (let i = 0; i < numberOfSWE.length; i++)
-	  {
-		  if (i === numberOfSWE.length - 1){
-			  process.stdout.write(numberOfSWE[i]);
-		  }
-		  else {
-		  process.stdout.write(numberOfSWE[i] + ', ');
-		  }
-	  }
-	  process.stdout.write('\n');
+    for (let i = 0; i < numberOfSWE.length; i += 1) {
+      if (i === numberOfSWE.length - 1) {
+        process.stdout.write(numberOfSWE[i]);
+      } else {
+        process.stdout.write(`${numberOfSWE[i]}, `);
+      }
+    }
+    process.stdout.write('\n');
   } catch (error) {
     throw new Error('Cannot load the database');
   }
