f413c823e306dcb9958b22ba6f10961e5bd5591f
root <bugfixdotexe@gmail.com>
2024-11-26 11:51:27 +0000

    solution to probem 2-read

---
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -0,0 +1,28 @@
+const fs = require('fs');
+
+function countStudents(pathToFile) {
+  if (pathToFile === null) {
+    throw Error('Cannot load the database');
+  }
+  try {
+    const numberOfCS = [];
+    const numberOfSWE = [];
+    let newData = [];
+    const data = fs.readFileSync(pathToFile, 'utf8');
+    newData = data.split('\n');
+    newData.forEach((value) => {
+      if (value.includes('CS')) {
+        numberOfCS.push(value.split(',')[0]);
+      }
+      if (value.includes('SWE')) {
+        numberOfSWE.push(value.split(',')[0]);
+      }
+    });
+    console.log(`Number of students: ${newData.length - 2}`);
+    console.log(`Number of students in CS: ${numberOfCS.length}. List: ${numberOfCS}`);
+    console.log(`Number of students in SWE: ${numberOfSWE.length}. List: ${numberOfSWE}`);
+  } catch (error) {
+    throw new Error('Cannot load the database');
+  }
+}
+module.exports = countStudents;
