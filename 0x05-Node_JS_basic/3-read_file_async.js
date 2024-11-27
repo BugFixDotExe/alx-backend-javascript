8601e021f08cfebcc5dadfcd1102be44144598e3
BugFixDotExe <bugfix.exe@gmail.com>
2024-11-27 10:52:07 +0100

    Solution to problem to problem 3

--- 0x05-Node_JS_basic/3-read_file_async.js
+++ 0x05-Node_JS_basic/3-read_file_async.js
@@ -1,41 +1,34 @@
-const fs = require('fs')
+const fs = require('fs');
 
 function countStudents(filePath) {
-  numberOfSWE = []
-  numberOfCS = []
   return new Promise((resolve, reject) => {
     try {
-      const data = fs.readFile(filePath, 'utf8');
-      const newData = data.split('\n');
-      newData.forEach((value) => {
-        if (value.includes('CS')) {
-          numberOfCS.push(value.split(',')[0]);
+      fs.readFile(filePath, 'utf8', (err, data) => {
+        if (err) {
+          reject(new Error('Cannot load the database'));
         }
-        if (value.includes('SWE')) {
-          numberOfSWE.push(value.split(',')[0]);
-        }
+        const lines = data.split('\n').filter((line) => line !== '');
+        const students = lines.slice(1);
+        const csStudents = [];
+        const sweStudents = [];
+
+        students.forEach((line) => {
+          const fields = line.split(',');
+          if (fields[fields.length - 1] === 'CS') {
+            csStudents.push(fields[0]);
+          }
+          if (fields[fields.length - 1] === 'SWE') {
+            sweStudents.push(fields[0]);
+          }
+        });
+        console.log(`Number of students: ${students.length}`);
+        console.log(`Number of students in CS: ${csStudents.length}. List: ${csStudents.join(', ')}`);
+        console.log(`Number of students in SWE: ${sweStudents.length}. List: ${sweStudents.join(', ')}`);
+        resolve();
       });
-      console.log(`Number of students: ${newData.length - 2}`);
-      process.stdout.write(`Number of students in CS: ${numberOfCS.length}. List: `);
-      for (let i = 0; i < numberOfCS.length; i += 1) {
-        if (i === numberOfCS.length - 1) {
-          process.stdout.write(`${numberOfCS[i]} `);
-        } else {
-          process.stdout.write(`${numberOfCS[i]}, `);
-        }
-      }
-      process.stdout.write('\n');
-      process.stdout.write(`Number of students in SWE: ${numberOfSWE.length}. List: `);
-      for (let i = 0; i < numberOfSWE.length; i += 1) {
-        if (i === numberOfSWE.length - 1) {
-          process.stdout.write(`${numberOfSWE[i]} `);
-        } else {
-          process.stdout.write(`${numberOfSWE[i]}, `);
-        }
-      }
-      process.stdout.write('\n');
     } catch (error) {
       reject(new Error('Cannot load the database'));
     }
   });
+}
 module.exports = countStudents;
