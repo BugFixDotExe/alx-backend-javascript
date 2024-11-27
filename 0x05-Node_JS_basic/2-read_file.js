1fbd4e5af0c72b0ec8d2f9383affd368e9b18111
BugFixDotExe <bugfix.exe@gmail.com>
2024-11-27 09:38:23 +0100

    debugging the program 2-read_file

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -19,24 +19,8 @@ function countStudents(pathToFile) {
       }
     });
     console.log(`Number of students: ${newData.length - 2}`);
-    process.stdout.write(`Number of students in CS: ${numberOfCS.length}. List: `);
-    for (let i = 0; i < numberOfCS.length; i += 1) {
-      if (i === numberOfCS.length - 1) {
-        process.stdout.write(`${numberOfCS[i]}`);
-      } else {
-        process.stdout.write(`${numberOfCS[i]}, `);
-      }
-    }
-    process.stdout.write('\n');
-    process.stdout.write(`Number of students in SWE: ${numberOfSWE.length}. List: `);
-    for (let i = 0; i < numberOfSWE.length; i += 1) {
-      if (i === numberOfSWE.length - 1) {
-        process.stdout.write(`${numberOfSWE[i]}`);
-      } else {
-        process.stdout.write(`${numberOfSWE[i]}, `);
-      }
-    }
-    process.stdout.write('\n');
+    console.log(`Number of students in CS: ${numberOfCS.length} List:`, numberOfCS.join(', '));
+    console.log(`Number of students in SWE: ${numberOfSWE.length} List:`, numberOfSWE.join(', '));
   } catch (error) {
     throw new Error('Cannot load the database');
   }
