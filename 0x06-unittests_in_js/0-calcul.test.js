2fd4bc1a5ca8452a7025cb50ea92051db95a6ed5
BugFixDotExe <bugfix.exe@gmail.com>
2024-11-28 11:00:05 +0100

    solution to setting up macho and writing a test suite

---
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -0,0 +1,16 @@
+const assert = require('assert');
+const calculateNumber = require('./0-calcul');
+
+describe('calculateNumber', () => {
+  it('should return the correct sum for integers', () => {
+    assert.strictEqual(calculateNumber(1, 2), 3);
+  });
+
+  it('should return the correct sum with rounded numbers', () => {
+    assert.strictEqual(calculateNumber(1, 3.7), 5);
+  });
+
+  it('should handle decimal numbers correctly', () => {
+    assert.strictEqual(calculateNumber(1.2, 3.7), 5);
+  });
+});
